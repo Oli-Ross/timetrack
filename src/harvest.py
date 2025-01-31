c248af570a28e00bacbc82a419b1e93a8d21d9d3
Oli Roß <oli.ross@goodresearch.com>
2025-01-31 20:45:27 +0100

    Remove unneeded db connection (already open)

--- src/harvest.py
+++ src/harvest.py
@@ -4,7 +4,6 @@ import urllib.request
 import urllib.parse
 
 from utils import get_task_length_in_mins
-from db_config import db
 from model import HarvestClient, HarvestProject, HarvestTask, HarvestMeta
 from env import EMAIL, HARVEST_TOKEN, HARVEST_ACCOUNT_ID, TASK_ID, PROJECT_ID
 
@@ -13,37 +12,36 @@ def sync_weekly_harvest_hours(KW=None):
     assert all(var is not None for var in (EMAIL, HARVEST_ACCOUNT_ID, HARVEST_TOKEN)), (
         "Environment variable for Harvest upload is missing."
     )
-    with db:
-        if KW:
-            today = datetime.fromisocalendar(datetime.now().year, KW, 2)
-        else:
-            today = datetime.now()
-        monday = today - timedelta(days=today.weekday())
-        friday = today + timedelta(days=(4 - today.weekday()))
-        fromDate = monday.strftime("%Y%m%d")
-        toDate = friday.strftime("%Y%m%d")
-        url = f"https://api.harvestapp.com/v2/reports/time/team?from={fromDate}&to={toDate}"
-        headers = {
-            "User-Agent": f"MyIntegration ({EMAIL})",
-            "Authorization": "Bearer " + str(HARVEST_TOKEN),
-            "Harvest-Account-Id": str(HARVEST_ACCOUNT_ID),
-        }
+    if KW:
+        today = datetime.fromisocalendar(datetime.now().year, KW, 2)
+    else:
+        today = datetime.now()
+    monday = today - timedelta(days=today.weekday())
+    friday = today + timedelta(days=(4 - today.weekday()))
+    fromDate = monday.strftime("%Y%m%d")
+    toDate = friday.strftime("%Y%m%d")
+    url = f"https://api.harvestapp.com/v2/reports/time/team?from={fromDate}&to={toDate}"
+    headers = {
+        "User-Agent": f"MyIntegration ({EMAIL})",
+        "Authorization": "Bearer " + str(HARVEST_TOKEN),
+        "Harvest-Account-Id": str(HARVEST_ACCOUNT_ID),
+    }
 
-        request = urllib.request.Request(url=url, headers=headers)
-        with urllib.request.urlopen(request, timeout=5) as response:
-            responseCode = response.getcode()
-            if responseCode != 200:
-                raise Exception("Request to Harvest failed.")
+    request = urllib.request.Request(url=url, headers=headers)
+    with urllib.request.urlopen(request, timeout=5) as response:
+        responseCode = response.getcode()
+        if responseCode != 200:
+            raise Exception("Request to Harvest failed.")
 
-            responseBody = response.read().decode("utf-8")
-            jsonResponse = json.loads(responseBody)
+        responseBody = response.read().decode("utf-8")
+        jsonResponse = json.loads(responseBody)
 
-        if not jsonResponse["results"]:
-            hours = 0.0
-        else:
-            hours = jsonResponse["results"][0]["total_hours"]
-        HarvestMeta.delete().execute()
-        HarvestMeta.create(hours=hours)
+    if not jsonResponse["results"]:
+        hours = 0.0
+    else:
+        hours = jsonResponse["results"][0]["total_hours"]
+    HarvestMeta.delete().execute()
+    HarvestMeta.create(hours=hours)
 
 
 def update_local_harvest_db():
@@ -66,26 +64,25 @@ def update_local_harvest_db():
         responseBody = response.read().decode("utf-8")
         jsonResponse = json.loads(responseBody)
 
-    with db:
-        for projectAssignment in jsonResponse["project_assignments"]:
-            clientId = str(projectAssignment["client"]["id"])
-            clientName = str(projectAssignment["client"]["name"])
-            client, _ = HarvestClient.get_or_create(clientId=clientId, name=clientName)
+    for projectAssignment in jsonResponse["project_assignments"]:
+        clientId = str(projectAssignment["client"]["id"])
+        clientName = str(projectAssignment["client"]["name"])
+        client, _ = HarvestClient.get_or_create(clientId=clientId, name=clientName)
 
-            projectId = str(projectAssignment["project"]["id"])
-            projectName = str(projectAssignment["project"]["name"])
-            project, _ = HarvestProject.get_or_create(
-                projectId=projectId, client=client, name=projectName
+        projectId = str(projectAssignment["project"]["id"])
+        projectName = str(projectAssignment["project"]["name"])
+        project, _ = HarvestProject.get_or_create(
+            projectId=projectId, client=client, name=projectName
+        )
+        for taskAssignment in projectAssignment["task_assignments"]:
+            taskId = str(taskAssignment["task"]["id"])
+            taskName = str(taskAssignment["task"]["name"])
+            HarvestTask.get_or_create(
+                taskId=taskId,
+                project=project,
+                client=client,
+                name=taskName,
             )
-            for taskAssignment in projectAssignment["task_assignments"]:
-                taskId = str(taskAssignment["task"]["id"])
-                taskName = str(taskAssignment["task"]["name"])
-                HarvestTask.get_or_create(
-                    taskId=taskId,
-                    project=project,
-                    client=client,
-                    name=taskName,
-                )
 
 
 def push_task(task):
@@ -141,7 +138,6 @@ def push_task(task):
 
 
 def pull():
-    with db:
-        sync_weekly_harvest_hours()
-        update_local_harvest_db()
-        print("Updated local db + weekly hours.")
+    sync_weekly_harvest_hours()
+    update_local_harvest_db()
+    print("Updated local db + weekly hours.")
